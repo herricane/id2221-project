6486a3bb08feba12355d539b0be73ac71cbe380f
chenhui <xu6648@gmail.com>
2020-10-06 18:12:06 +0200

    add spotify_caller.py

---
+++ spotify_caller.py
@@ -0,0 +1,21 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotify_credentials import *
+
+
+class SpotifyClient(object):
+    def __init__(self):
+        auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+        self.sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    def get_info(self, track_id):
+        track_info = self.sp.track(track_id=track_id)
+        track_name = track_info['name']
+
+        artist_id = track_info['artist'][0]['id']
+        artist_info = self.sp.artist(artist_id=artist_id)
+        artist_name = artist_info['name']
+
+        genres = artist_info['genres']
+
+        return {'name': track_name, 'artist': artist_name, 'genres': genres}
