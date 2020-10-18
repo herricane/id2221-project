3b819bd8a949a47061db3ae4e27fd07321363083
Jie Huang <hj98331@gmail.com>
2020-10-18 17:47:16 +0200

    no message

--- spotify_caller.py
+++ spotify_caller.py
@@ -12,10 +12,13 @@ class SpotifyClient(object):
         track_info = self.sp.track(track_id=track_id)
         track_name = track_info['name']
 
-        artist_id = track_info['artist'][0]['id']
+        artist_id = track_info['artists'][0]['id']
         artist_info = self.sp.artist(artist_id=artist_id)
         artist_name = artist_info['name']
 
-        genres = artist_info['genres']
+        try:
+            genres = artist_info['genres'][0] # first genre
+        except:
+            genres = []
 
         return {'name': track_name, 'artist': artist_name, 'genres': genres}
